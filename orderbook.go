5711d67b030379831360d55ae45857bad39e321a
Gabriel Navas <gabnavas.dev@gmail.com>
2026-07-23 20:48:32 -0300

    feat: created a order book struct

---
+++ orderbook.go
@@ -0,0 +1,44 @@
+package main
+
+type Order struct {
+	Size      float64 // 0.5 BTC
+	Limit     *Limit  // Limit{Price: 65000.0, Order1, }
+	Timestamp int64
+}
+
+type Limit struct {
+	Price float64 // 65000.0
+
+	// cada Order é de uma pessoa, por exemplo, mas todas partilham do mesmo price que pode ser de compra ou venda
+	// Order{Size:0.5},Order{Size:0.2},Order{Size:0.5},
+	Orders []*Order
+
+	// imagino que total de volume aqui de order
+	TotalVolume float64
+}
+
+type Orderbook struct {
+	// Eu vendo até X valor por isso
+	// Do menor para o maior, quem cobra menos tem prioridade
+	Asks []*Limit
+
+	// Eu compro até X valor por isso
+	// Do maior para o menor, quem paga mais, tem prioridade
+	// a compra é feita aqui quando Bid >= que Ask
+	Bids []*Limit
+}
+
+// exemplo:
+// order e limit anda sempre junto então. Eu quero vender 0.5BTC a 65000.0.
+// Então vou ter um limit dentro de ask de 65000 e uma order de 0.5BTC.
+// Orderbook{
+//   Asks: []*Limit{
+//     &Limit{
+//       Price: 65000.0,
+//       Orders: []*Order{
+//         &Order{ Size: 0.5, Limit: <ponteiro pro Limit acima> },
+//       },
+//       TotalVolume: 0.5,
+//     },
+//   },
+// }
