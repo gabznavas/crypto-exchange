884002e1fb656f21ab49eb89a1b200ce7ca93461
Gabriel Navas <gabnavas.dev@gmail.com>
2026-07-23 23:33:03 -0300

    feat: new limit, new order and add order

--- orderbook.go
+++ orderbook.go
@@ -1,9 +1,20 @@
 package main
 
+import "time"
+
 type Order struct {
 	Size      float64 // 0.5 BTC
+	Bid       bool    // se é compra
 	Limit     *Limit  // Limit{Price: 65000.0, Order1, }
 	Timestamp int64
+}
+
+func NewOrder(bid bool, size float64) *Order {
+	return &Order{
+		Size:      size,
+		Bid:       bid,
+		Timestamp: time.Now().UnixNano(),
+	}
 }
 
 type Limit struct {
@@ -15,6 +26,19 @@ type Limit struct {
 
 	// imagino que total de volume aqui de order
 	TotalVolume float64
+}
+
+func NewLimit(price float64) *Limit {
+	return &Limit{
+		Price:  price,
+		Orders: []*Order{},
+	}
+}
+
+func (l *Limit) AddOrder(o *Order) {
+	o.Limit = l
+	l.Orders = append(l.Orders, o)
+	l.TotalVolume += o.Size
 }
 
 type Orderbook struct {
--- orderbook_test.go
+++ orderbook_test.go
@@ -1,6 +1,17 @@
 package main
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
+
+func TestLimit(t *testing.T) {
+	l := NewLimit(10_000)
+	buyOrder := NewOrder(true, 0.5)
+	l.AddOrder(buyOrder)
+
+	fmt.Printf("%+v\n", l)
+}
 
 func TestOrderbook(t *testing.T) {
 
